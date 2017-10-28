715439db46bc9fd4b5222d6ad255e5e0fb34d14c
sasaki <info@ftp.gawanative.com>
2017-10-28 21:55:44 +0900

    bash化

--- .travis.zip.sh
+++ .travis.zip.sh
@@ -1,10 +1,11 @@
-#!/bin/csh
-if (! $?TRAVIS_TAG ) then
+#!/bin/bash
+if [ "x${TRAVIS_TAG}" = "x" ]; then
     echo "TRAVIS_TAG undefined"
-    exit
-endif
+    exit 9
+fi
+
 rm -rf bin/zip
 mkdir bin/zip
 pushd bin
-zip -r zip/${TRAVIS_TAG}.zip Release
+zip -r zip/${TRAVIS_TAG}.zip Release || exit
 popd
